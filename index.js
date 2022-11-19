e656282b53991e82046620d0ffbb2bd26bdd5051
zuiaiminmin <118539977+zuiaiminmin@users.noreply.github.com>
2022-11-19 12:04:33 +0800

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ var S = {
     if (i !== -1) {
       S.UI.simulate(decodeURI(action).substring(i + 3));
     } else {
-      S.UI.simulate('|#countdown 3||闵闵|祝你|生日|快乐|❤|');
+      S.UI.simulate('|#countdown 3||闵闵|祝你|生日|快乐|❤|永远|爱你|(づ￣3￣)|');
     }
 
     S.Drawing.loop(function () {
